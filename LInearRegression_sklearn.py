a0dbfb5cb6ec196e9444d9282c48df07238f80f1
Nishkarsh Tripathi <50888917+NishkarshTripathi@users.noreply.github.com>
2019-09-12 22:23:46 +0530

    Add files via upload

---
+++ LInearRegression_sklearn.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_boston
+
+boston = load_boston()
+
+df_x = pd.DataFrame(boston.data, columns=boston.feature_names)
+df_y = pd.DataFrame(boston.target)
+
+reg = linear_model.LinearRegression()
+
+x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=4)
+
+reg.fit(x_train, y_train)
+
+print(reg.predict(x_test))
