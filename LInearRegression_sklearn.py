2aefe5b93459aa8772dfdea208eae2a9b1186418
Nishkarsh Tripathi <50888917+NishkarshTripathi@users.noreply.github.com>
2019-09-12 23:10:13 +0530

    Update LInearRegression_sklearn.py

--- LInearRegression_sklearn.py
+++ LInearRegression_sklearn.py
@@ -3,8 +3,17 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_boston
-
+import seaborn as sb
+import matplotlib.pyplot as plt
 boston = load_boston()
+
+#df = pd.DataFrame(boston)
+#print(df_x.head())
+#c = df.corr()
+
+##Check the correlation using heatmap
+#sb.heatmap(c, vmax=1, vmin=0, square=True)
+#plt.show
 
 df_x = pd.DataFrame(boston.data, columns=boston.feature_names)
 df_y = pd.DataFrame(boston.target)
@@ -15,4 +24,4 @@ x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, r
 
 reg.fit(x_train, y_train)
 
-print(reg.predict(x_test))
+print(reg.score(x_test))
